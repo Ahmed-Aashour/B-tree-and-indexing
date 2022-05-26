a7babfc753a3e7beea0a5536f1959b1d9d730a92
Ahmed Hassan <ahmed__hassan@outlook.com>
2022-05-26 18:31:49 +0200

    Search Engine Implementation

--- src/main/java/org/dslabs/SearchEngine.java
+++ src/main/java/org/dslabs/SearchEngine.java
@@ -1,37 +1,129 @@
 package org.dslabs;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
 
 public class SearchEngine implements ISearchEngine {
 
+    IBTree<String, HashMap<Integer, Integer>> index = new BTree<>(2);
+
     @Override
     public void indexWebPage(String filePath) {
-        // TODO Auto-generated method stub
-
+        try {
+            File inputFile = new File(filePath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            NodeList docs = doc.getElementsByTagName("doc");
+            for (int temp = 1; temp < docs.getLength(); temp++) {
+                int docId = Integer.parseInt(docs.item(temp).getAttributes().getNamedItem("id").getNodeValue());
+                String[] words = docs.item(temp).getTextContent().split("\\s+");
+                for (String word : words) {
+                    HashMap<Integer, Integer> docFreq = this.index.search(word);
+                    if (docFreq != null) {
+                        if (docFreq.containsKey(docId)) {
+                            docFreq.put(docId, docFreq.get(docId) + 1);
+                        } else {
+                            docFreq.put(docId, 1);
+                        }
+                    } else {
+                        docFreq = new HashMap<>();
+                        docFreq.put(docId, 1);
+                        this.index.insert(word, docFreq);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void indexDirectory(String directoryPath) {
-        // TODO Auto-generated method stub
-
+        File dir = new File(directoryPath);
+        File[] directoryListing = dir.listFiles();
+        if (directoryListing != null) {
+            for (File child : directoryListing) {
+                if (child.isFile()) {
+                    this.indexWebPage(child.getAbsolutePath());
+                } else if (child.isDirectory()) {
+                    this.indexDirectory(child.getAbsolutePath());
+                }
+            }
+        }
     }
 
     @Override
     public void deleteWebPage(String filePath) {
-        // TODO Auto-generated method stub
-
+        try {
+            File inputFile = new File(filePath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            NodeList docs = doc.getElementsByTagName("doc");
+            for (int temp = 1; temp < docs.getLength(); temp++) {
+                int docId = Integer.parseInt(docs.item(temp).getAttributes().getNamedItem("id").getNodeValue());
+                String[] words = docs.item(temp).getTextContent().split("\\s+");
+                for (String word : words) {
+                    HashMap<Integer, Integer> docFreq = this.index.search(word);
+                    if (docFreq != null) {
+                        docFreq.remove(docId);
+                        if (docFreq.isEmpty()) {
+                            this.index.delete(word);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<ISearchResult> searchByWordWithRanking(String word) {
-        // TODO Auto-generated method stub
-        return null;
+        List<ISearchResult> results = new ArrayList<>();
+        HashMap<Integer, Integer> docFreq = this.index.search(word);
+        if (docFreq != null) {
+            for (Integer docId : docFreq.keySet()) {
+                SearchResult result = new SearchResult();
+                result.setId(docId.toString());
+                result.setRank(docFreq.get(docId));
+                results.add(result);
+            }
+        }
+        return results;
     }
 
     @Override
     public List<ISearchResult> searchByMultipleWordWithRanking(String sentence) {
-        // TODO Auto-generated method stub
-        return null;
+        List<ISearchResult> results = new ArrayList<>();
+        String[] words = sentence.split("\\s+");
+        HashMap<Integer, Integer> docFreq = new HashMap<>();
+        for (String word : words) {
+            HashMap<Integer, Integer> temp = this.index.search(word);
+            if (temp != null) {
+                for (Integer docId : temp.keySet()) {
+                    if (docFreq.containsKey(docId)) {
+                        docFreq.put(docId, docFreq.get(docId) + temp.get(docId));
+                    } else {
+                        docFreq.put(docId, temp.get(docId));
+                    }
+                }
+            }
+        }
+        for (Integer docId : docFreq.keySet()) {
+            SearchResult result = new SearchResult();
+            result.setId(docId.toString());
+            result.setRank(docFreq.get(docId));
+            results.add(result);
+        }
+        return results;
     }
 
 }
--- src/main/java/org/dslabs/SearchResult.java
+++ src/main/java/org/dslabs/SearchResult.java
@@ -2,28 +2,26 @@ package org.dslabs;
 
 public class SearchResult implements ISearchResult {
 
+    private String id;
+    private int rank;
     @Override
     public String getId() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.id;
     }
 
     @Override
     public void setId(String id) {
-        // TODO Auto-generated method stub
-
+        this.id = id;
     }
 
     @Override
     public int getRank() {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.rank;
     }
 
     @Override
     public void setRank(int rank) {
-        // TODO Auto-generated method stub
-
+        this.rank = rank;
     }
 
 }
