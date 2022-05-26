3a1190c74ab8c3233e69e6625e6cdbab37770550
Ahmed Hassan <ahmed__hassan@outlook.com>
2022-05-26 18:32:25 +0200

    Adding Search Engine Test

--- src/main/java/org/dslabs/Main.java
+++ src/main/java/org/dslabs/Main.java
@@ -1,18 +1,16 @@
 package org.dslabs;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        BTree<Integer, String> btree = new BTree<>(2);
-        Integer[] keys = {1, 5, 4, 3, 2 ,10, 7, 8, 9, 6};
-        for (int i = 0; i < keys.length; i++){
-            btree.insert(keys[i], keys[i].toString());
+        ISearchEngine searchEngine = new SearchEngine();
+        searchEngine.indexDirectory("datasets/");
+        List<ISearchResult> searchResults = searchEngine.searchByMultipleWordWithRanking("slab serif");
+        for (ISearchResult searchResult : searchResults) {
+            System.out.println(searchResult.getId() + " " + searchResult.getRank());
         }
-        btree.delete(10);
-        btree.InorderBTree(btree.getRoot());
     }
 
 }
